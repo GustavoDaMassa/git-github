973c2fe4796aeb91839f3cccd7d1d60f168f8459
Gustavo Henrique <gustavo.pereira@discente.ufg.br>
2024-02-19 11:15:31 -0300

    remove a line

--- Materia.java
+++ Materia.java
@@ -1,20 +1,18 @@
 public class Materia {
 
-    private int mediafinal = 6;
     public int quantdeprovas;
 
-    public Materia(int quantdeprovas){
+    public Materia(int quantdeprovas) {
         this.quantdeprovas = quantdeprovas;
 
     }
 
-    public static int Somatorio(int...n){
-        int soma=0;
-        for(int i : n){
-            soma+=i;
+    public static int Somatorio(int... n) {
+        int soma = 0;
+        for (int i : n) {
+            soma += i;
         }
         return soma;
     }
 
-    
 }
