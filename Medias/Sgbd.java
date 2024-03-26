8d6996968522c9db78750f331d42b9718dd2ace6
Gustavo Henrique <gustavo.pereira@discente.ufg.br>
2024-03-26 11:34:44 -0300

    implementando medias de sgbd

--- Medias/Sgbd.java
+++ Medias/Sgbd.java
@@ -1,6 +1,135 @@
 package Medias;
 
+
+import java.util.Scanner;
+import java.math.*;
+
 public class Sgbd {
 
-    
+    public static void main(String[] args) {
+
+        double menor = 0;
+        double media = 0;
+        double avmedia = 0;
+        double nnotas = 0; // numero de atividades 
+        double avnotas = 0; // numero de avaliaçoes
+        double notas[] = new double[16];
+        double avaliacao[] = new double[2];
+
+        for (int i = 0; i < notas.length; i++) {
+            notas[i] = 11;
+        }
+
+        System.out.println("Digite as notas das suas atividades (apos a ultima digite 12):");
+
+        Scanner scanner = new Scanner(System.in);
+
+        for (int j = 0; j < notas.length; j++) {
+
+            notas[j] = scanner.nextDouble();
+            if (notas[j] == 12)
+                break;
+            nnotas = j + 1;
+
+        }
+
+        double soma = 0;
+
+        for (int i = 0; notas[i] != 12; i++) {
+            soma += notas[i];
+        }
+        
+        //System.out.println(soma);
+         media = soma / nnotas;
+
+        System.out.println("media geral das atividades: " + media);
+
+        double desc = nnotas / 4;
+
+        System.out.println("Deseja descartar o teto de 25% (1) ou o chao de 25% (2) das notas?");
+
+        int n = scanner.nextInt();
+
+        if (n == 1)
+            desc = arredondarC(desc);
+        else if (n == 2)
+            desc = arredondarF(desc);
+        else {
+            System.out.println("Opcao invalida. Sera feito o arrendondamento matematico");
+            desc = arredondar(desc);
+        }
+
+        System.out.printf("foram descartadas %.1lf notas.\n", desc);
+        System.out.println("foram descartadas " + desc + " notas.");
+
+        int m = 0;
+        for (int i = 0; i < desc; i++) {
+
+            menor = notas[m];
+            for (int j = 0; j < notas.length; j++) {
+                if (notas[j] < menor)
+                    menor = notas[j];
+                m = j;
+            }
+            notas[m] = 11;
+
+        }
+
+        soma = 0;
+        for (int i = 0; i < notas.length; i++) {
+            if (notas[i] != 12 && notas[i] != 11)
+                soma += notas[i];
+            System.out.println(soma);
+
+        }
+
+        System.out.println(soma);
+        media = soma / (nnotas - desc);
+
+        System.out.printf("nota das atividades com 25 porcento descartado: %.2f ", media);
+
+        for (int i = 0; i < avaliacao.length; i++) {
+            avaliacao[i] = 11;
+        }
+        
+        System.out.println("Digite as notas das suas atividades (apos a ultima digite 12):");
+
+
+        for (int j = 0; j < avaliacao.length; j++) {
+
+            notas[j] = scanner.nextDouble();
+            if (avaliacao[j] == 12)
+                break;
+            avnotas = j + 1;
+
+        }
+
+        double avsoma = 0;
+
+        for (int i = 0; avaliacao[i] != 12; i++) {
+            avsoma += avaliacao[i];
+        }
+        
+        //System.out.println(avsoma);
+         avmedia = avsoma / avnotas;
+
+        System.out.println("media geral das avaliaçoes: " + avmedia);
+        System.out.println();
+        
+
+        System.out.printf("media geral da disciplina: " + ((0.4 * media) +(0.6*avmedia)));
+
+    }
+
+    private static double arredondarF(double desc) {
+        return Math.floor(desc);
+    }
+
+    private static double arredondarC(double desc) {
+        return Math.ceil(desc);
+    }
+
+    private static double arredondar(double desc) {
+        return Math.round(desc);
+    }
 }
---
+++ q
@@ -0,0 +1,89 @@
+[33mcommit fc7853efec1c85af2056368f0134c65c796370f0[m[33m ([m[1;36mHEAD -> [m[1;32mmaster[m[33m, [m[1;31mmedia/master[m[33m, [m[1;32mgustavo[m[33m)[m
+Author: Gustavo Henrique <gustavo.pereira@discente.ufg.br>
+Date:   Tue Mar 26 10:59:54 2024 -0300
+
+     renomeando pastas e iniciando o projeoto de medias para a disciplina de sgbd
+
+[33mcommit 271579798bd3dbb5266174179f1923b4e34216ae[m
+Author: Gustavo Henrique <gustavo.pereira@discente.ufg.br>
+Date:   Wed Feb 21 11:30:22 2024 -0300
+
+    excluindo arquivo nao encapsulado
+
+[33mcommit 2b0858f132fdf480fecdc5ba4d9dcc68658ba64b[m
+Author: Gustavo Henrique <gustavo.pereira@discente.ufg.br>
+Date:   Wed Feb 21 11:25:18 2024 -0300
+
+    Colocando uma pasta
+
+[33mcommit 97bd8acf282592f731deab6b500616a8887a4c9c[m
+Author: Gustavo Henrique <gustavo.pereira@discente.ufg.br>
+Date:   Wed Feb 21 11:06:15 2024 -0300
+
+    correção no readme
+
+[33mcommit 0ea81a8de72a95138407e92403ab3948e3f789fb[m
+Author: Gustavo Henrique <gustavo.pereira@discente.ufg.br>
+Date:   Wed Feb 21 10:49:32 2024 -0300
+
+     adicionando o READ-ME ao projeto
+
+[33mcommit 6b730e8b4102c7133c783813c3c5a59f7f545965[m
+Author: Gustavo Henrique <gustavo.pereira@discente.ufg.br>
+Date:   Mon Feb 19 12:40:52 2024 -0300
+
+    alteraçao no read-me
+
+[33mcommit 13357773cdf0ded7f9a1ad95bae2267991959e17[m
+Author: GustavoDaMassa <116303095+GustavoDaMassa@users.noreply.github.com>
+Date:   Mon Feb 19 12:36:54 2024 -0300
+
+    Create README.md
+
+[33mcommit 8f9cc54e6f81f2013e2b8a049f177b9fb04c9645[m
+Author: Gustavo Henrique <gustavo.pereira@discente.ufg.br>
+Date:   Mon Feb 19 12:11:00 2024 -0300
+
+    teste com a branch secundaria
+
+[33mcommit bd04cb50a648442b2fdb0ee8c6bf9ad13ff8ad8d[m
+Author: Gustavo Henrique <gustavo.pereira@discente.ufg.br>
+Date:   Mon Feb 19 12:05:35 2024 -0300
+
+    commit branch
+
+[33mcommit 5e6fb5dc2c94d2f106029f6ad3ce7a43e9e4ee0c[m
+Author: Gustavo Henrique <gustavo.pereira@discente.ufg.br>
+Date:   Mon Feb 19 11:44:27 2024 -0300
+
+    comentário
+
+[33mcommit 335af53355c5e02602585f5c48ec88555c0e45c5[m
+Author: Gustavo Henrique <gustavo.pereira@discente.ufg.br>
+Date:   Mon Feb 19 11:32:16 2024 -0300
+
+    todos?
+
+[33mcommit 9332cb979d4102f8b34fb719550ffe868d6f6b4a[m
+Author: Gustavo Henrique <gustavo.pereira@discente.ufg.br>
+Date:   Mon Feb 19 11:31:29 2024 -0300
+
+    commit m.java only
+
+[33mcommit 312cd80f594bb2671f28f38ad695cdfaf8d9341f[m
+Author: Gustavo Henrique <gustavo.pereira@discente.ufg.br>
+Date:   Mon Feb 19 11:26:01 2024 -0300
+
+    add line example
+
+[33mcommit 973c2fe4796aeb91839f3cccd7d1d60f168f8459[m
+Author: Gustavo Henrique <gustavo.pereira@discente.ufg.br>
+Date:   Mon Feb 19 11:15:31 2024 -0300
+
+    remove a line
+
+[33mcommit 6f6108b1bf38c0a6f7314b87104e9af0dc2ceb38[m
+Author: Gustavo Henrique <gustavo.pereira@discente.ufg.br>
+Date:   Mon Feb 19 11:04:23 2024 -0300
+
+    First commit
